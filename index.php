cda6b11e15f26157065b82c78d30c2038380ea92
ningtenglei <ningtenglei@dongmengwl.com>
2021-11-16 16:20:45 +0800

    新字段

--- index.php
+++ index.php
@@ -14,15 +14,16 @@ try {
 		];
     }
 
-    $sql = "SELECT id, comment_count, current, exec_at FROM quote WHERE symbol = '" . $symbol .  "'  ORDER BY exec_at";
+    $sql = "SELECT id, comment_count, current,low,high, exec_at FROM quote WHERE symbol = '" . $symbol .  "'  ORDER BY exec_at";
 
     foreach($dbh->query($sql) as $row) {
     	$labels[] = $row['exec_at'];
 		$data[] = [
 			"x" => $row['exec_at'],
+			"low" => $row['low'],
+			"high" => $row['high'],
 			"comment" => $row['comment_count'],
 			"current" => $row['current'],
-
 		];
     }
     $dbh = null;
@@ -32,8 +33,6 @@ try {
 }
 
 ?>
-
-
 <!doctype html>
 <html lang="zh-CN">
 <head>
@@ -49,25 +48,53 @@ try {
 		    type: 'line',
 		    data: {
 		        labels: labels,
-		        datasets: [{
-		            label: '评论数',
-		            data: data,
-		            backgroundColor: [
-	                'rgba(54, 162, 235, 0.2)',
-	            	],
-		            parsing: {
-		                yAxisKey: 'comment'
-		            }
-		        }, {
-		            label: '价格',
-		            data: data,
-		            backgroundColor: [
-	                'rgba(255, 99, 132, 1)',
-	            	],
-		            parsing: {
-		                yAxisKey: 'current'
-		            }
-		        }]
+		        tension: 0.1,
+		        datasets: [
+		        	{
+			            label: '价格',
+			            data: data,
+			            backgroundColor: 'rgba(255, 99, 132, 1)',
+	    				borderColor: 'rgba(255, 99, 132, 1)',
+	    				borderWidth: 1,
+			            parsing: {
+			                yAxisKey: 'current',
+			            },
+			            yAxisID: 'current',
+		        	},
+		        	{
+			            label: '最高价',
+			            data: data,
+			            backgroundColor: 'rgba(255, 0, 0, 0.2)',
+	    				borderColor: 'rgba(255, 0, 0, 0.2)',
+	    				borderWidth: 1,
+			            parsing: {
+			                yAxisKey: 'high'
+			            },
+			            yAxisID: 'current',
+		        	},
+		        	{
+			            label: '最低价',
+			            data: data,
+			            backgroundColor: 'rgba(0, 128, 0, 0.2)',
+	    				borderColor: 'rgba(0, 128, 0, 0.2)',
+	    				borderWidth: 1,
+			            parsing: {
+			                yAxisKey: 'low'
+			            },
+			            yAxisID: 'current',
+		        	},
+		        	{
+			            label: '评论数',
+			            data: data,
+			            backgroundColor: 'rgba(54, 162, 235, 1)',
+	    				borderColor: 'rgba(54, 162, 235, 1)',
+	    				borderWidth: 1,
+			            parsing: {
+			                yAxisKey: 'comment',
+			            },
+			            yAxisID: 'comment',
+		        	}
+		        ],
 		    },
 		};
 
