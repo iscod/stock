5d4dfd2f8474a63b6f6b83e408a2763853917522
ningtenglei <ningtenglei@dongmengwl.com>
2021-11-19 14:44:22 +0800

    sql

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ try {
                         yAxisID: 'comment',
                     },
                     {
-                        label: '成交单',
+                        label: '成交量',
                         data: data,
                         backgroundColor: 'rgba(153, 102, 255, 0.5)',
                         borderColor: 'rgba(153, 102, 255, 0.5)',
